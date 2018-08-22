1fdb7037340aae43d414b679d162769267e4929f
cain910772 <cain910772@yahoo.com>
2018-08-21 20:31:58 -0500

    trying

--- src/js/listToDom.js
+++ src/js/listToDom.js
@@ -1,4 +1,43 @@
+let APIManager =require("./apiManager")
 
+
+
+const putOnDom = Object.create(null, {
+    "myCards": {
+        value: (myRes) => {
+            return `<span>
+            <div id ="tryMe"class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Location:${myRes.location}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Location Type:${myRes.locationType}</h6>
+    <p class="card-text">Review: ${myRes.review} </p>
+    <button id="workPls" >Delete</button>
+  
+    </span>
+
+</div>`
+
+
+        },
+    "domLoop": {
+        value: (result) => {
+            for (let i = 0; i < result.length; i++) {
+                const element = result[i]
+                return putOnDom.myCards(element)
+
+
+            }
+        },
+        "deleteMe":{
+            value:(id)=>{
+            document.getElementById("workPls").click(apiManager.deleteEntry(id))
+
+        }
+}}
+
+}})
+
+module.exports = putOnDom
 
 //
 //     $("#listButton").click(makeNewList)
